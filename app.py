2876df1d13d0f3e8c92d4c15aed5e8a96b1c0e5b
Sarika Sarvia <sarika.sarvia1@gmail.com>
2019-02-21 17:54:08 +0000

    initial commit

--- app.py
+++ app.py
@@ -24,29 +24,20 @@ def home():
     if request.method == 'POST':
         if 'find_task_submit' in request.form:
             find_task_data = request.form.to_dict()
-            delete_task = db_api.move_task(find_task_data)
-    #        db_api_response = requests.get("http://127.0.0.1:5000/tasks")
-    #        api_data = db_api_response.json()
-    #        return render_template("home.html", title="Homepage", **locals())
+            delete_task = db_api.delete_task(find_task_data)
+            db_api_response = requests.get("http://127.0.0.1:5000/tasks")
+            api_data = db_api_response.json()
+            return render_template("home.html", title="Homepage", **locals())
 
-        elif 'sort_submit' in request.form:
-            if 'sort_radio' == "1": #priority
-                db_api_response = requests.get("http://127.0.0.1:5000/tasks")
-            elif 'sort_radio' == "2": #date
-                db_api_response = requests.get("http://127.0.0.1:5000/tasks")
-                print(db_api_response)
-                api_data = []
-                for row in db_api_response:
-                    api_data.append({"id":row[0],"title":row[1],"description":row[2],"date_created":row[3],"date_due":row[4],"status":row[5],"priority":row[6]})
-                conn.close()
-                print(api_data)
-                jsonify(api_data)
-                return render_template("home.html", title="Homepage", **locals())
         else:
             form_data = request.form.to_dict()
             db_api.add_task(form_data)
 
 
+#        description = form_data["description"]
+#        title = form_data["title"]
+#            due_date = form_data["due_date"]
+#            due_created = form_data["date_created"]
 #        json_data = jsonify(form_data)
 #        return json_data
 #        post_db_response = requests.post("http://127.0.0.1:5000/tasks", data = json_data)
@@ -67,16 +58,11 @@ def new_task():
 @app.route("/tasks", methods=['GET', 'POST'])
 def tasks():
     if request.method == 'GET':
-        dummy_api_data = db_api.select_all_priority()
+        dummy_api_data = db_api.select_all()
         return jsonify(dummy_api_data)
 
 #    elif request.method == 'POST':
 #        db_api.add_task(post_db_response)
-
-@app.route("/completed_tasks")
-def completed_tasks():
-    api_data = db_api.select_all_done()
-    return render_template("completed_tasks.html", title="Completed Tasks", **locals())
 
 
 
